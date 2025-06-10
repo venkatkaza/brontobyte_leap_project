babe0b360dba4993491223e2b69f74e098a47312
venkatkaza <34800418+venkatkaza@users.noreply.github.com>
2025-06-11 02:55:47 +0530

    Update thought_seeds.py

--- thought_seeds.py
+++ thought_seeds.py
@@ -1,4 +1,4 @@
-from config.prompts import load_prompt
+from prompts import load_prompt
 from langchain_groq import ChatGroq
 
 llm = ChatGroq(model="mixtral-8x7b-32768")  # You can also use OpenAI, Claude etc.
